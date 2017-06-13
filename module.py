8dbe6423e7e11c6f3dd12ae89726ba713f5f55a5
Louis-Saglio <louis.saglio@ynov.com>
2017-06-13 23:11:52 +0200

    Vérification de l'existance de l'url dans tux search

--- module.py
+++ module.py
@@ -1,2 +1,21 @@
+#! /bin/python
+
+from http.client import HTTPConnection, ResponseNotReady 
+
+
 def write_search_url(site, search):
     return "https://" + site + ".com/search?q=" + '+'.join(search.split())
+
+
+def is_404(url):
+    connection = HTTPConnection(url)
+    try:
+        code = connection.request("GET", url)
+        is_404 = False
+    except:
+        is_404 = True
+    return is_404
+
+if __name__ == "__main__":
+    print(check_url_404("www.google.com"))
+
--- plugins.py
+++ plugins.py
@@ -1,12 +1,15 @@
 #! /bin/python
 
 from webbrowser import open_new_tab
-from module import write_search_url
+from module import *
 
 
 def search(site: str, search: str):
-    open_new_tab(write_search_url(site, search))
+    url = write_search_url(site, search)
+    if is_404(url):
+        print(f"Désolé, {site} n'accèpte pas ce type de recherche.")
+        return
+    open_new_tab(url)
 
 
 commandes = {'search': search}
-
