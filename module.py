885fb56894f5379f7b279267e5f8741db103b2f0
Louis-Saglio <louis.saglio@ynov.com>
2017-06-30 10:25:41 +0200

    [NEW FEATURE] tux search slant ...

--- module.py
+++ module.py
@@ -1,7 +1,8 @@
 #! /bin/python
 
 from http.client import HTTPConnection, ResponseNotReady 
-
+from urllib.error import URLError
+from urllib.request import urlopen
 
 def write_search_url(site, search):
     
@@ -38,6 +39,26 @@ def write_search_url(site, search):
 
     if site == "twitter":
         join_char = " "
+
+    if site == "slant":
+        extension = ".co"
+        get_var = "query"
+        join_char = ' '
     
     return protocole + "://" + site + extension + '/' + slash + start_get + get_var + equal + join_char.join(search.split())
+
+
+def check404(url):
+    is_404 = False
+    try:
+        urlopen(url)
+    except URLError:
+        is_404 = True
+    return is_404
+
+
+if __name__ == '__main__':
+    url = write_search_url("google", "rien")
+    print(url)
+    print(check404(url))
 
